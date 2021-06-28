82cbda3be6d7c52383c7bd104d28e237d8d009e9
Eduardo Ramos <eduardo.auramos@gmail.com>
2021-06-27 21:42:16 -0300

    add project meme me v2

---
+++ MemeMe-v2/MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Eduardo Ramos on 12/06/21.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalText: UIImage
+    let memedImage: UIImage
+}
